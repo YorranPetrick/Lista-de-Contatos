1af2473048acdf5910bd8be1d7eeb8257e9001b1
Yorran Petrick <yorranpetrick27@gmail.com>
2024-07-22 20:12:05 -0300

    Inicio da criação da verificação dos campos de formulário

--- app/src/main/java/com/yorran/listadecontatos/AddContactActivity.kt
+++ app/src/main/java/com/yorran/listadecontatos/AddContactActivity.kt
@@ -1,20 +1,45 @@
 package com.yorran.listadecontatos
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.yorran.listadecontatos.databinding.ActivityAddContactBinding
 
 class AddContactActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddContactBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_contact)
+        binding = ActivityAddContactBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.addContactBtn.setOnClickListener {
+
+            val firstName = binding.FirstName.text.toString()
+            val familyName = binding.FamilyName.text.toString()
+            val dddPhone = binding.DDDFone.text.toString()
+            val telephone = binding.Telephone.text.toString()
+
+            verificarCampos(firstName, familyName, dddPhone, telephone )
+        }
     }
+
+    private fun verificarCampos(firstName: String, familyName: String, dddPhone: String, telephone: String){
+        if(firstName.isEmpty() || familyName.isEmpty() || dddPhone.isEmpty() || telephone.isEmpty()){
+
+            Toast.makeText(this, "Todos os Campos Precisam ser Preenchidos", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
