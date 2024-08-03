dca7d698ea4d0a67f703708c9062c3253c3e5277
Yorran Petrick <yorranpetrick27@gmail.com>
2024-08-03 10:38:07 -0300

    Alterando o adapter para passar as informações do Banco de dados para a Activity de atualização de usuários

--- app/src/main/java/com/yorran/listadecontatos/AtualizarUsuarioActitity.kt
+++ app/src/main/java/com/yorran/listadecontatos/AtualizarUsuarioActitity.kt
@@ -5,16 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.yorran.listadecontatos.databinding.ActivityAtualizarUsuarioBinding
+import com.yorran.listadecontatos.databinding.ActivityContactBinding
 
 class AtualizarUsuarioActitity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAtualizarUsuarioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_atualizar_usuario)
+        binding = ActivityAtualizarUsuarioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        completarCampos(binding)
+
+
+    }
+
+    private fun completarCampos(binding : ActivityAtualizarUsuarioBinding){
+        //Variaveis para pegar os dados do putExtra criado no Adapter
+        val firstName = intent.getStringExtra("firstName")
+        val familyNme = intent.getStringExtra("familyName")
+        val ddd = intent.getStringExtra("ddd")
+        val telephone = intent.getStringExtra("telephone")
+
+        //Passando os dados para os campos
+
+        binding.FirstName.setText(firstName)
+        binding.FamilyName.setText(familyNme)
+        binding.DDDFone.setText(ddd)
+        binding.Telephone.setText(telephone)
+
+
     }
 }
--- app/src/main/java/com/yorran/listadecontatos/adapter/Adapter.kt
+++ app/src/main/java/com/yorran/listadecontatos/adapter/Adapter.kt
@@ -1,9 +1,11 @@
 package com.yorran.listadecontatos.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.yorran.listadecontatos.AtualizarUsuarioActitity
 import com.yorran.listadecontatos.databinding.ActivityContactBinding
 import com.yorran.listadecontatos.models.User
 
@@ -20,10 +22,29 @@ class Adapter(private val context: Context, private val listaContato: MutableLis
 
     //Responsável por exibir os itens criados pelo metodo onCreateViewHolder
     override fun onBindViewHolder(holder: ContatosViewHolder, position: Int) {
+        val firstName = listaContato[position].firstName
+        val familyName = listaContato[position].familyName
+        val ddd = listaContato[position].ddd
+        val telephone = listaContato[position].telephone
+
         //Recuperando as informações contidas na listaContatos e passando aos campos correspondentes
-        holder.idFirstNamee.text = listaContato[position].firstName
-        holder.idNomeFamily.text = listaContato[position].familyName
-        holder.idNumber.text = "(${listaContato[position].ddd}) ${listaContato[position].telephone}"
+        holder.idFirstNamee.text = firstName
+        holder.idNomeFamily.text = familyName
+        holder.idNumber.text = ("${(ddd)} $telephone")
+
+        //Criando evento de clique no icone de atualizar
+        holder.idEditBtn.setOnClickListener {
+            val intent = Intent(context, AtualizarUsuarioActitity::class.java)
+            //Passando os dados para a proxima tela com o PutExtra
+            intent.putExtra("firstName", firstName)
+            intent.putExtra("familyName",familyName)
+            intent.putExtra("ddd", ddd)
+            intent.putExtra("telephone", telephone)
+            intent.putExtra("id", listaContato[position].id)
+            //Inicializado a Activity
+            context.startActivity(intent)
+
+        }
     }
 
     override fun getItemCount(): Int = listaContato.size
